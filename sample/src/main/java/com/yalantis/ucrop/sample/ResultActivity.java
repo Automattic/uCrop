e2e6a247d11d82c74fa504eb286bc5f0a4ed37bb
Adam Grzybkowski <agrzybkowski@outlook.com>
2025-02-13 21:29:59 +0100

    Make the sample app work again (#11)
    * Use compile&target SDK version 35
    
    * Remove write/read external storage permissions from the sample app
    
    * Fix saving the cropped image by adjusting the code to Scoped Storage

--- sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
+++ sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
@@ -1,49 +1,37 @@
 package com.yalantis.ucrop.sample;
 
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.content.FileProvider;
+import androidx.core.content.ContextCompat;
 
 import com.yalantis.ucrop.util.BitmapLoadUtils;
 import com.yalantis.ucrop.view.UCropView;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.channels.FileChannel;
+import java.io.OutputStream;
 import java.util.Calendar;
-import java.util.List;
-
-import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
-import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+import java.util.Locale;
 
 /**
  * Created by Oleksii Shliama (https://github.com/shliama).
@@ -51,8 +39,6 @@ import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
 public class ResultActivity extends BaseActivity {
 
     private static final String TAG = "ResultActivity";
-    private static final String CHANNEL_ID = "3000";
-    private static final int DOWNLOAD_NOTIFICATION_ID_DONE = 911;
 
     public static void startWithUri(@NonNull Context context, @NonNull Uri uri) {
         Intent intent = new Intent(context, ResultActivity.class);
@@ -75,10 +61,6 @@ public class ResultActivity extends BaseActivity {
                 uCropView.getOverlayView().setShowCropGrid(false);
                 uCropView.getOverlayView().setDimmedColor(Color.TRANSPARENT);
 
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP && "content".equals(uri.getScheme())) {
-                    TextView textViewExifWarning = findViewById(R.id.text_view_content_warning);
-                    textViewExifWarning.setVisibility(View.VISIBLE);
-                }
             } catch (Exception e) {
                 Log.e(TAG, "setImageUri", e);
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
@@ -112,7 +94,7 @@ public class ResultActivity extends BaseActivity {
             height = options.outHeight;
         }
 
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar(findViewById(R.id.toolbar));
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -142,33 +124,33 @@ public class ResultActivity extends BaseActivity {
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_STORAGE_WRITE_ACCESS_PERMISSION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    saveCroppedImage();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_STORAGE_WRITE_ACCESS_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveCroppedImage();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
     private void saveCroppedImage() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
             requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     getString(R.string.permission_write_storage_rationale),
                     REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
         } else {
             Uri imageUri = getIntent().getData();
-            if (imageUri != null && imageUri.getScheme().equals("file")) {
+            if (imageUri != null && "file".equals(imageUri.getScheme())) {
                 try {
-                    copyFileToDownloads(getIntent().getData());
+                    saveImageToExternalStorage(getIntent().getData());
+                    Toast.makeText(this, R.string.notification_image_saved, Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     Toast.makeText(ResultActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     Log.e(TAG, imageUri.toString(), e);
                 }
-            } else if (BitmapLoadUtils.hasContentScheme(imageUri)){
+            } else if (BitmapLoadUtils.hasContentScheme(imageUri)) {
                 Toast.makeText(ResultActivity.this, getString(R.string.toast_already_saved), Toast.LENGTH_SHORT).show();
                 finish();
             } else {
@@ -177,77 +159,53 @@ public class ResultActivity extends BaseActivity {
         }
     }
 
-    private void copyFileToDownloads(Uri croppedFileUri) throws Exception {
-        String downloadsDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-        String filename = String.format("%d_%s", Calendar.getInstance().getTimeInMillis(), croppedFileUri.getLastPathSegment());
+    public void saveImageToExternalStorage(Uri croppedFileUri) throws IOException {
+        String filename = String.format(Locale.getDefault(),
+                "%d_%s", Calendar.getInstance().getTimeInMillis(),
+                croppedFileUri.getLastPathSegment());
 
-        File saveFile = new File(downloadsDirectoryPath, filename);
-
-        FileInputStream inStream = new FileInputStream(new File(croppedFileUri.getPath()));
-        FileOutputStream outStream = new FileOutputStream(saveFile);
-        FileChannel inChannel = inStream.getChannel();
-        FileChannel outChannel = outStream.getChannel();
-        inChannel.transferTo(0, inChannel.size(), outChannel);
-        inStream.close();
-        outStream.close();
-
-        showNotification(saveFile);
-        Toast.makeText(this, R.string.notification_image_saved, Toast.LENGTH_SHORT).show();
-        finish();
-    }
-
-    private void showNotification(@NonNull File file) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        Uri fileUri = FileProvider.getUriForFile(
-                this,
-                getString(R.string.file_provider_authorities),
-                file);
-
-        intent.setDataAndType(fileUri, "image/*");
-
-        List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(
-                intent,
-                PackageManager.MATCH_DEFAULT_ONLY);
-        for (ResolveInfo info : resInfoList) {
-            grantUriPermission(
-                    info.activityInfo.packageName,
-                    fileUri, FLAG_GRANT_WRITE_URI_PERMISSION | FLAG_GRANT_READ_URI_PERMISSION);
-        }
-
-        NotificationCompat.Builder notificationBuilder;
-        NotificationManager notificationManager = (NotificationManager) this
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(createChannel());
-            }
-            notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        ContentResolver resolver = getContentResolver();
+        Uri imageCollection;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            imageCollection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
         } else {
-            notificationBuilder = new NotificationCompat.Builder(this);
+            imageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
         }
 
-        notificationBuilder
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(getString(R.string.notification_image_saved_click_to_preview))
-                .setTicker(getString(R.string.notification_image_saved))
-                .setSmallIcon(R.drawable.ic_done)
-                .setOngoing(false)
-                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
-                .setAutoCancel(true);
-        if (notificationManager != null) {
-            notificationManager.notify(DOWNLOAD_NOTIFICATION_ID_DONE, notificationBuilder.build());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, filename);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/*");
+        contentValues.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH,
+                    Environment.DIRECTORY_PICTURES + File.separator + "uCropSample");
+            contentValues.put(MediaStore.Images.Media.IS_PENDING, 1);
+        }
+
+        Uri destinationUri = resolver.insert(imageCollection, contentValues);
+        if (destinationUri != null) {
+            try (InputStream inputStream = resolver.openInputStream(croppedFileUri);
+                 OutputStream outputStream = resolver.openOutputStream(destinationUri)) {
+
+                if (inputStream == null || outputStream == null) {
+                    throw new IOException("Failed to open input or output stream");
+                }
+
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    contentValues.clear();
+                    contentValues.put(MediaStore.Images.Media.IS_PENDING, 0);
+                    resolver.update(destinationUri, contentValues, null, null);
+                }
+            }
+        } else {
+            throw new IOException("Failed to create new MediaStore record.");
         }
     }
-
-    @TargetApi(Build.VERSION_CODES.O)
-    public NotificationChannel createChannel() {
-        int importance = NotificationManager.IMPORTANCE_LOW;
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, getString(R.string.channel_name), importance);
-        channel.setDescription(getString(R.string.channel_description));
-        channel.enableLights(true);
-        channel.setLightColor(Color.YELLOW);
-        return channel;
-    }
-
 }
